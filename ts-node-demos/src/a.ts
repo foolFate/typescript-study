ce0e842d93991804bdcfa755e3933ada146f91e9
王琦(wangqi2) <wangqi2@mochasoft.com.cn>
2025-01-06 01:56:07 +0800

    feat: 创建ts-study项目

---
+++ ts-node-demos/src/a.ts
@@ -0,0 +1,21 @@
+export default {};
+
+let a: number = 123;
+a = "asdasd";
+
+let b = 456;
+const c = 666;
+
+type A = 1 | 2;
+
+let x1: A = 2;
+
+interface Form {
+  username: string;
+  password: string;
+}
+let form: Form = {
+  username: "",
+  password: "",
+  x: 123
+};
---
+++ ts-node-demos/src/b.ts
@@ -0,0 +1,3 @@
+export default {};
+
+let a: number = 123;
